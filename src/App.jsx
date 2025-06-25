25f8f115ca3eaf1a076a88f41da5f00f82f453bd
Akash Jadhav <152849719+akashjadhav09@users.noreply.github.com>
2025-06-25 18:52:40 +0530

    Design completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import HomePageNarrative from './features/home/HomePageNarrative';
 
 function App() {
 
+  let title = "Unforgettable, Websites, Brands & Visuals for Bold Visionaries.";
+  let body = "We’re not your typical design agency. Founded in 2014, we’re a collective of designers, developers, and strategists who believe great digital experiences should be beautiful, functional, and human-centered. From startups to global brands, we help ambitious businesses stand out.We’re not your typical design agency. Founded in 2014, we’re a collective of designers, developers, and strategists who believe great digital experiences should be beautiful, functional, and human-centered. From startups to global brands, we help ambitious businesses stand out.";
+  let imgsrc = "./public/assets/images/homepageNarr1.jpg"
   return (
     <>
-      <p className='text-violet-500'>Hello World!</p>
-    </>
-   
+     <HomePageNarrative titleText={title} bodyText={body} imgSrc={imgsrc}/>
+    </>   
   )
 }
 
---
+++ src/features/home/HomePageNarrative.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function HomePageNarrative({titleText, bodyText, imgSrc}){
+    return(
+        <div className="home-narrative-wrapper__main">
+            <div className="home-narrative-wrapper__outer">
+                <div className="home-narrative-wrapper__inner w-full py-8 px-20">
+                    <div className="narrative-content-wrapper__outer flex gap-28 border-b border-gray-400 py-16">
+                        <div className="narrative-content-wrapper w-1/2">
+                            <div className="title-text text-5xl">{titleText}</div>
+                            <div className="body-text text-lg mt-8">{bodyText}</div>
+                            <button className="border border-black rounded-full mt-8 p-4 hover:bg-[#b0e600] hover:border-transparent">Know More About Us</button>
+                        </div>
+
+                        <div className="narrative-image-wrapper w-[40%]">
+                            <div className="image-wrapper__inner w-[95%]">
+                                <img className="h-full w-full rounded-3xl" src={imgSrc} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
