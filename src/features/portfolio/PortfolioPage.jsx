2f183d7ad80a4276baea7d7b942f91a4bac06bdc
Akash Jadhav <152849719+akashjadhav09@users.noreply.github.com>
2025-06-30 23:01:07 +0530

    image slider development.

--- src/features/portfolio/PortfolioPage.jsx
+++ src/features/portfolio/PortfolioPage.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-import PageIntroBanner from "../../shared/component/PageIntroBanner";
+// import PageIntroBanner from "../../shared/component/PageIntroBanner";
 import ImageSlider from "../../shared/component/ImageSlider";
 
 export default function PortfolioPage(){
     return(
         <div className="portfolio-page__main">
-            <PageIntroBanner 
+            {/* <PageIntroBanner 
                 title = "We Design & Build Digital Experiences That Move the Needle"
                 body = "We’re not your average design shop. We’re a strategic creative studio that blends jaw-dropping aesthetics with ruthless functionality. Since 2017, we’ve helped 100+ startups, agencies, and scale-ups transform their ideas into high-performing websites, apps, and brands that stand out and convert."
                 src = "./public/assets/images/homepageNarr1.jpg"
-            />
+            /> */}
             <ImageSlider />
         </div>
     )
